d14b3927a4ecc1b5433d9aaa774987274d6831a6
johnson19750717 <johnson.he@mobiledaily.cn>
2016-07-17 22:37:54 +0800

    update transaction flow

--- src/main/java/cn/hisforce/domain/TransactionFlow.java
+++ src/main/java/cn/hisforce/domain/TransactionFlow.java
@@ -2,16 +2,32 @@ package cn.hisforce.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * Created by Johnson on 2016/7/17.
  */
 @Entity
+@Table(name = "AngelGuiderTransactionFlow")
 public class TransactionFlow implements Serializable {
     private static final long serialVersionUID = 1793374721034690047L;
     @Id
     private Long id;
+    private String flowNo;
+    private Long uid;
+    private String accountNo;
+    private Long accountId;
+    private Date createDate = new Date();
+    private String comment;
+    private Integer transactionCode;
+    private Double amount;
+    private Long shareId;
+    private Double currentBalance;
+
+    public TransactionFlow() {
+    }
 
     public Long getId() {
         return id;
@@ -19,5 +35,85 @@ public class TransactionFlow implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getFlowNo() {
+        return flowNo;
+    }
+
+    public void setFlowNo(String flowNo) {
+        this.flowNo = flowNo;
+    }
+
+    public Long getUid() {
+        return uid;
+    }
+
+    public void setUid(Long uid) {
+        this.uid = uid;
+    }
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Integer getTransactionCode() {
+        return transactionCode;
+    }
+
+    public void setTransactionCode(Integer transactionCode) {
+        this.transactionCode = transactionCode;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Long getShareId() {
+        return shareId;
+    }
+
+    public void setShareId(Long shareId) {
+        this.shareId = shareId;
+    }
+
+    public Double getCurrentBalance() {
+        return currentBalance;
+    }
+
+    public void setCurrentBalance(Double currentBalance) {
+        this.currentBalance = currentBalance;
     }
 }
