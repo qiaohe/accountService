2d1b3e9f69feb143b5da587eae0e6478343e27ab
johnson19750717 <johnson.he@mobiledaily.cn>
2016-07-18 21:19:27 +0800

    update transaction flow

---
+++ src/main/java/cn/hisforce/domain/transaction/Keys.java
@@ -0,0 +1,10 @@
+package cn.hisforce.domain.transaction;
+
+/**
+ * Created by johnson on 2016-07-18.
+ */
+public final class Keys {
+    public static String transactionFlowKey() {
+        return "transaction.flow.incr";
+    }
+}
